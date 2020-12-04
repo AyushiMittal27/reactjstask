127fb339a6f787e38529cf69368ca47b21007a10
Ayushi Mittal <45877325+AyushiMittal27@users.noreply.github.com>
2020-12-04 19:36:09 +0530

    Update controller.js

--- client/src/controller.js
+++ client/src/controller.js
@@ -1,19 +1,20 @@
 import axios from "axios";
-export const saveUser = async (num, code) => {
+export const saveUser = async (num, code,otp) => {
   const response = await axios.post("http://localhost:5000/api/phonenumber/auth", {
     phonenum: num,
     countrycode: code,
+    otp
   });
   console.log(response.data.msg);
+  return response.data;
 };
 
-export const sendOTP = async (num, code, otp) => {
+export const sendOTP = async (num, code) => {
   const response = await axios.post(
     "http://localhost:5000/api/otp",
     {
       phonenum: num,
       countrycode: code,
-      otp,
     }
   );
   return response.data;
